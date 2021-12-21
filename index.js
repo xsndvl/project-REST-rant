6ebf1710e336c09d8ec12ac630177d875304d003
Joswag Sandoval <sergiojr100@outlook.com>
2021-12-20 19:40:41 -0600

    adding home page view

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use("/places", require("./controllers/places"))
 
 
 app.get('/', (req, res) =>{
-    res.send('<h1 style="color:red;" >Hello World</h1>')
+    // res.send('<h1 style="color:red;" >Hello World</h1>')
+    res.render('Home')
 })
 
 app.get('*', (req, res) =>{
---
+++ views/Home.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('./Default')
+
+function Home(){
+    return(
+        <Def>
+            <main>
+                <h1>HOME</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = Home
