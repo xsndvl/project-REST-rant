36d09ceea43faef980d5222e49de1adbba0eab1d
Joswag Sandoval <sergiojr100@outlook.com>
2021-12-27 20:11:21 -0600

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,7 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) =>{
-    let places = [{
-        name: 'H-Thai-ML',
-        city: "Seattle",
-        state: "WA",
-        cuisines: "Thai, Pan-Asian",
-        pic: "/images/restaurant1.jpg"
-    },{
-        name: "Coding Cat Cafe",
-        city: "Phoenix",
-        state: "AZ",
-        cuisines: "Coffee, Bakery",
-        pic: "/images/restaurant2.jpg"
-    }]
-
     res.render("places/Index", {places})
 })
 
@@ -24,6 +11,16 @@ router.get("/new",(req, res) =>{
 
 router.post("/", (req, res)=>{
     console.log(req.body)
+    if (!req.body.pic){
+        req.body.pic = "http://placekitten.com/400/400"
+    }
+    if (!req.body.city){
+        req.body.city = "Anytown"
+    }
+    if (!req.body.state){
+        req.body.state = "USA"
+    }
+    places.push(req.body)
     res.redirect('/places')
 })
 
---
+++ models/places.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: "Seattle",
+    state: "WA",
+    cuisines: "Thai, Pan-Asian",
+    pic: "/images/restaurant1.jpg"
+},{
+    name: "Coding Cat Cafe",
+    city: "Phoenix",
+    state: "AZ",
+    cuisines: "Coffee, Bakery",
+    pic: "/images/restaurant2.jpg"
+}]
+
