5e944d92f065f1c80dbe76ced2d622dfb50fb062
Joswag Sandoval <sergiojr100@outlook.com>
2021-12-28 22:41:23 -0600

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -33,7 +33,21 @@ router.get("/:id", (req, res)=>{
         res.status(404).render("error404")
     }
     else{
-        res.render("places/show", {place: places[id]})
+        res.render("places/show", {place: places[id], id: req.params.id})
+    }
+})
+
+router.delete("/:id", (req, res)=>{
+    let id = Number(req.params.id)
+    if (isNaN(id)){
+        res.status(404).render('error404')
+    }
+    else if (!places[id]){
+        res.status(404).render("error404")
+    }
+    else{
+        places.splice(id,1)
+        res.redirect("/places")
     }
 })
 
--- views/places/Show.jsx
+++ views/places/Show.jsx
@@ -1,11 +1,36 @@
 const React = require('react')
 const Def = require('../Default')
 
-function Show(){
+function Show(data){
     return(
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <h1>{data.place.name}</h1>
+
+                <img src={data.place.pic} alt={data.place.name}/>
+
+                <div className="rating">
+                    <h2>Rating</h2>
+                    <h3>Not Rated</h3>
+                </div>
+                <div className="description">
+                    <h2>Description</h2>
+                    <h3>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h3>
+                </div>
+                <div>
+                    <h2>Comments</h2>
+                    <h3>No comments yet</h3>
+                </div>
+
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                    Edit
+                </a>
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
+
             </main>
         </Def>
     )
