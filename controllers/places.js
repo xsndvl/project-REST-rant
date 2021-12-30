bb8e9644a6593d7c2695912c24957052ea083d81
Joswag Sandoval <sergiojr100@outlook.com>
2021-12-29 18:43:17 -0600

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -37,6 +37,49 @@ router.get("/:id", (req, res)=>{
     }
 })
 
+router.get('/:id/edit', (req, res)=>{
+    let id = Number(req.params.id)
+    if (isNaN(id)){
+        res.status(404).render('error404')
+    }
+    else if (!places[id]){
+        res.status(404).render("error404")
+    }
+    else{
+        res.render("places/edit", {place: places[id], id: req.params.id})
+    }
+})
+
+//update
+router.put('/:id', (req, res) => {
+    console.log(req.body)
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        console.log(req.body)
+        // Save the new data into places[id]
+        places[req.params.id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+  })
+
 router.delete("/:id", (req, res)=>{
     let id = Number(req.params.id)
     if (isNaN(id)){
--- views/places/Edit.jsx
+++ views/places/Edit.jsx
@@ -1,2 +1,37 @@
 const React = require('react')
 const Def = require('../Default')
+
+function Edit_form(data){
+    return(
+        <Def>
+            <main>
+                <h1>Edit Place</h1>
+                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                    <div className="form-group">
+                        <label htmlFor="name">Place Name</label>
+                        <input className="form-control" name="name" id="name" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input className="form-control" type="url" name="pic" id="pic"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">Place City</label>
+                        <input className="form-control" type="text" name="city" id="city"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">Place State</label>
+                        <input className="form-control" name="state" id="state"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cuisines">Place Cuisines</label>
+                        <input className="form-control" name="cuisines" id="cuisines" required/>
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Add Place"/>
+                </form>
+            </main>
+        </Def>
+    )
+}
+
+module.exports=Edit_form
