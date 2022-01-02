9e7909339a3f86b1b36c05b51f8f29d3f4216db1
Joswag Sandoval <sergiojr100@outlook.com>
2022-01-02 13:32:30 -0600

    worked on index page for mongo

--- controllers/places.js
+++ controllers/places.js
@@ -29,7 +29,14 @@ router.post("/", (req, res)=>{
 })
 
 router.get("/:id", (req, res)=>{
-    res.send("GET /places/:id stub")
+    db.Place.findById(req.params.id)
+    .then(place => {
+        res.render("places/show", {place})
+    })
+    .catch(err => {
+        console.log("err", err)
+        res.render("error404")
+    })
 })
 
 router.get('/:id/edit', (req, res)=>{
